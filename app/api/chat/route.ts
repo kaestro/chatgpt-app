8609a0f1f560ad41e52f189c16d92099a709c4f8
kaestro <didme654@snu.ac.kr>
2024-09-16 18:18:01 +0900

    Changed api response for debugging purpose

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import OpenAI from 'openai'; // 'OpenAI'를 기본 내보내기로 가져옴
+import OpenAI from 'openai';
 
-// OpenAI API 설정
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
@@ -22,7 +21,9 @@ export async function POST(req: NextRequest) {
     const reply = response.choices[0].message.content;
     return NextResponse.json({ reply });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: 'OpenAI API call failed' }, { status: 500 });
+    return NextResponse.json(
+        { error: `your message "${message}" is waiting for server to be ready`},
+        { status: 500 }
+    )
   }
 }
